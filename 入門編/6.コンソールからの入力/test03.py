7d3c8c347898effa65b25d0a04150fb9d300f0f9
username <username@example.com>
2020-03-15 13:12:15 +0900

    test03.pyを追加

---
+++ 入門編/6.コンソールからの入力/test03.py
@@ -0,0 +1,11 @@
+# print('あなたの名前を教えてください。')
+ 
+# your_name = input('>> ')
+ 
+# print(your_name)
+
+print('あなたの名前を教えてください。')
+ 
+your_name = input('>> ')
+ 
+print('こんにちは！ {} さん！'.format(your_name))
